48949474d8899aaa6571514396a6082764e3dddb
9um4 <sweetenpotato@outlook.com>
2023-06-21 13:53:21 +0900

    feat: initialize major job selection ui

--- src/main/java/io/github/gum4/jobplugin/JobHandlers/MajorJobHandler.java
+++ src/main/java/io/github/gum4/jobplugin/JobHandlers/MajorJobHandler.java
@@ -4,18 +4,25 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class MajorJobHandler {
     private static final Component selectMajorJobUIName = Component.text("[", NamedTextColor.WHITE)
             .append(Component.text("LIN", NamedTextColor.AQUA))
             .append(Component.text("] 메인 직업을 선택해 주세요.", NamedTextColor.WHITE))
             .decoration(TextDecoration.ITALIC, false);
-    private static Inventory selectMajorJobUI = Bukkit.createInventory(null, 27, selectMajorJobUIName);
+    private static final Inventory selectMajorJobUI = Bukkit.createInventory(null, 27, selectMajorJobUIName);
     public MajorJobHandler(){
         // initialize selectMajorJobUI
         if (selectMajorJobUI.isEmpty()){
-
+            for (int i = 0; i < 27; i++) {
+                ItemStack selectMajorJobIcon = new ItemStack(Material.PAPER, 1);
+                // todo: set the custom model data, lore and display name of items
+                selectMajorJobUI.setItem(i, selectMajorJobIcon);
+            }
         }
     }
 }
