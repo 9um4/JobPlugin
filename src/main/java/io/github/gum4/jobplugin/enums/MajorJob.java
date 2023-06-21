dff6bc1289d267a4663bcb949eef4c890615472f
9um4 <sweetenpotato@outlook.com>
2023-06-21 14:49:18 +0900

    feat: define majorJobMap

---
+++ src/main/java/io/github/gum4/jobplugin/enums/MajorJob.java
@@ -0,0 +1,7 @@
+package io.github.gum4.jobplugin.enums;
+
+public enum MajorJob {
+    WARRIOR,
+    ARCHER,
+    MAGICIAN
+}
--- src/main/java/io/github/gum4/jobplugin/jobsystemhandlers/MajorJobHandler.java
+++ src/main/java/io/github/gum4/jobplugin/jobsystemhandlers/MajorJobHandler.java
@@ -1,5 +1,6 @@
 package io.github.gum4.jobplugin.jobsystemhandlers;
 
+import io.github.gum4.jobplugin.enums.MajorJob;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
@@ -12,12 +13,16 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class MajorJobHandler {
     private static final Component selectMajorJobUIName = Component.text("[", NamedTextColor.WHITE)
             .append(Component.text("LIN", NamedTextColor.AQUA))
             .append(Component.text("] 메인 직업을 선택해 주세요.", NamedTextColor.WHITE))
             .decoration(TextDecoration.ITALIC, false);
     private static final Inventory selectMajorJobUI = Bukkit.createInventory(null, 27, selectMajorJobUIName);
+    private static HashMap<UUID, MajorJob> majorJobMap = new HashMap<>();
     public MajorJobHandler(){
         // initialize selectMajorJobUI
         if (selectMajorJobUI.isEmpty()){
@@ -44,17 +49,17 @@ public class MajorJobHandler {
 
         // on warrior selection
         if ( (clickedSlot % 9) <= 2 ){
-            // todo: set player's job to warrior
+            majorJobMap.put(player.getUniqueId(), MajorJob.WARRIOR);
             // todo: give player basic weapons for warrior
         }
         // on archer selection
         else if ( (clickedSlot % 9) <= 5 ) {
-            // todo: set player's job to archer
+            majorJobMap.put(player.getUniqueId(), MajorJob.ARCHER);
             // todo: give player basic weapons for archer
         }
         // on magician selection
         else {
-            // todo: set player's job to magician
+            majorJobMap.put(player.getUniqueId(), MajorJob.MAGICIAN);
             // todo: give player basic weapons for magician
         }
         event.getInventory().close();
