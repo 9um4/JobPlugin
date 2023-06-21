23e743612331886fd892b45a0b78eec53ba893e6
9um4 <sweetenpotato@outlook.com>
2023-06-21 13:26:40 +0900

    [INIT] Initialize Project

---
+++ src/main/java/io/github/gum4/jobplugin/JobInitializer.java
@@ -0,0 +1,17 @@
+package io.github.gum4.jobplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JobInitializer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
